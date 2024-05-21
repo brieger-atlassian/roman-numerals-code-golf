cc8517b0587dab240b65b42bebdcab4c19f7f1dc
Brennan Rieger <brieger@atlassian.com>
2024-05-20 17:28:06 -0700

    better var name

--- run_code_golf.sh
+++ run_code_golf.sh
@@ -1,14 +1,14 @@
 
-non_matching_count=0
+invalid_cases=0
 
 while IFS=, read -r column1 column2; do
     output=$(go run roman_numerals.go $column1)
     if [ "$output" != "$column2" ]; then
-        ((non_matching_count++))
+        ((invalid_cases++))
     fi
 done < "roman_numerals.csv"
 
-echo $non_matching_count
+echo $invalid_cases
 
 NUM_CHARS=$(cat roman_numerals.go | wc | awk '{print $3}')
 echo $NUM_CHARS
